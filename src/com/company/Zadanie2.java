e68fdded4c3052854105d45489ea8ff0ff0738a7
madejlukas <lukasz.madej25@gmail.com>
2021-09-05 23:12:20 +0200

    Pliki do gita

---
+++ src/com/company/Zadanie2.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Zadanie2 {
+    public static void main(String[] args) {
+        Scanner liczba = new Scanner(System.in);
+        System.out.println("Podaj 1 liczbę:");
+        int liczba1 = liczba.nextInt();
+        System.out.println("Podaj 2 liczbę:");
+        int liczba2 = liczba.nextInt();
+        int liczba3 = Math.subtractExact(liczba1,liczba2);
+
+
+        System.out.println("Wynik odejmowania = " + liczba3);
+    }
+}
---
+++ src/com/company/Zadanie3.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Zadanie3 {
+    public static void main(String[] args) {
+        int liczba1 = 4;
+        double wynik = Math.sqrt(liczba1);
+
+        System.out.println(wynik);
+    }
+}
