fe86dce3829e11ca14ac64452f9d38f2713f6d11
madejlukas <lukasz.madej25@gmail.com>
2021-09-20 22:53:36 +0200

    Zadanie 3

--- src/com/company/Zadanie3.java
+++ src/com/company/Zadanie3.java
@@ -1,10 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Zadanie3 {
     public static void main(String[] args) {
-        int liczba1 = 4;
-        double wynik = Math.sqrt(liczba1);
+        Scanner user = new Scanner(System.in);
 
-        System.out.println(wynik);
+        System.out.println("Podaj pierwszą liczbę");
+        int liczba1 = user.nextInt();
+        System.out.println("Podaj drugą liczbę");
+        int liczba2 = user.nextInt();
+
+        int wynik = liczba1 * liczba2;
+
+        System.out.println("Wynik mnożenia = " + wynik);
     }
 }
